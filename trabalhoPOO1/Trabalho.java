1b9e8f967f0f2f0832574441a76c1a93afaaeec0
juliocesarmlk <113483466+juliocesarmlk@users.noreply.github.com>
2023-03-27 22:08:55 -0300

    Delete trabalhoPOO1 directory

--- trabalhoPOO1/Trabalho.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class Trabalho {
-   public static void main(String[] args){
-      Tarefa tarefa1 = new Tarefa("Estudar para a prova", "Resumir todos os slides so trabalho");
-      ListaTarefas Lista_de_tarefas = new ListaTarefas();
-      Lista_de_tarefas.addFirst(tarefa1);
-   }
-}

ccd946d27552fdd796ce3d0d9eeb34bafa224461
juliocesarmlk <113483466+juliocesarmlk@users.noreply.github.com>
2023-04-03 19:40:03 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Menu{
 
    Gerenciador mantemTarefas = new Gerenciador();
 
-   Scanner leitor = new Scanner(System.in);
    private  int resposta;
    
    public void setResposta(int resposta){
@@ -16,8 +15,8 @@ public class Menu{
 
    public void inicio(){
       while(true) {
-        System.out.println("Digite o numero que aparecer antes da opcao que voce quer acessar:\n\n1 Criar tarefa\n2 Concluir tarefa\n3 Exibir tarefas pendentes\n4 Exibir tarefas concluidas");
-        resposta = leitor.nextInt();
+        Uteis.print("Digite o numero que aparecer antes da opcao que voce quer acessar:\n\n1 Criar tarefa\n2 Concluir tarefa\n3 Exibir tarefas pendentes\n4 Exibir tarefas concluidas");
+        resposta = Uteis.lerInt();
         escolha();
       }
    }
@@ -25,6 +24,7 @@ public class Menu{
    public void escolha(){
       switch(resposta){
          case 1: 
+            //chamar metodo para limpar tela 
             mantemTarefas.addTarefa();
             break;
          case 2:
@@ -34,10 +34,12 @@ public class Menu{
             mantemTarefas.exibirTarefas();
             break;
          case 4:
-            System.out.println("aaa");
+            mantemTarefas.exibirConcluidas();
             break;
+            // cirar opção de saída do código
          default:
-            System.out.println("Burro");
+            Uteis.print("Burro");
+            // consertar isso
             break;
       }
    

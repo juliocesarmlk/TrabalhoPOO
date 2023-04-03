085cc214827ee7f0d4abc5946a3f13be84d76c92
juliocesarmlk <113483466+juliocesarmlk@users.noreply.github.com>
2023-04-03 19:39:36 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,11 @@ public class Main {
    public static void main(String[]args){
       Gerenciador gerencia = new Gerenciador();
       Menu in = new Menu();
+      Persistencia p = new Persistencia();
+      public String "C:/Users/1-22-11619/Documents/trabalho poo tentativa 2/usuarios";
+
       
       in.inicio();
+      p.criarArquivos();
    }
 }

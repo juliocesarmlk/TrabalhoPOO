e64dc874fc9813d2b3886c6d34dac9b49571b96d
juliocesarmlk <113483466+juliocesarmlk@users.noreply.github.com>
2023-04-03 19:39:13 -0300

    Update Gerenciador.java

--- Gerenciador.java
+++ Gerenciador.java
@@ -1,56 +1,65 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 public class Gerenciador{
 
-   private int cont = 0;
    private int concluindo;
    ArrayList<Tarefa> pendentes = new ArrayList<Tarefa>();
-   Scanner leitor = new Scanner(System.in);
+   ArrayList<Tarefa> concluidas = new ArrayList<Tarefa>();
    
-   public void setCont(int cont){
-      this.cont = cont;
-   }
    public void setConcluindo(int concluindo){
       this.concluindo = concluindo;
-   }
-   public int getCont(){
-      return cont;
    }
    public int getConcluindo(){
       return concluindo;
    }
    
-    public void addTarefa(){  
-      System.out.println("Digite o titulo da sua tarefa");
-      String titulo = leitor.nextLine();
-      System.out.println("Digite a descricao da sua tarefa");
-      String descricao = leitor.nextLine();
+    public void addTarefa(){
+      Uteis.print("Digite o titulo da sua tarefa");
+      String titulo = Uteis.lerString();
+      Uteis.print("Digite a descricao da sua tarefa");
+      String descricao = Uteis.lerString();
       Tarefa tarefa = new Tarefa(titulo, descricao);                
       pendentes.add(tarefa); 
+      // adicionar ao arquivo do usuario
    }
    
    public void exibirTarefas() {
-
+   int index = 0;
     for (Tarefa t : pendentes) {
-        System.out.println("Tarefa: " + cont);
-        System.out.println("Titulo: " + t.getTitulo());
-        System.out.println("Descricao: " + t.getDescricao());
-        System.out.println("Data de inicio: " + t.getDataInicio());
-        System.out.println("Status: " + t.getStatus());
-        System.out.println("-----------------------");
+        Uteis.print("Número da tarefa: " + index++);
+        Uteis.print("Titulo: " + t.getTitulo());
+        Uteis.print("Descricao: " + t.getDescricao());
+        Uteis.print("Data de inicio: " + t.getDataInicio());
+        Uteis.print("Status: " + t.getStatus());
+        Uteis.print("-----------------------");
     }
 }
    public void concluirTarefa(){
       if(pendentes.isEmpty()){
-         System.out.println("Nao ha tarefas para serem concluidas");
+         Uteis.print("Nao ha tarefas para serem concluidas");
       }
       else{
-         System.out.println("Digite o numero da tarefa que voce concluiu");
-         concluindo = leitor.nextInt();
-         for (Tarefa t : pendentes){
-            System.out.println(cont);   
-         }
+         exibirTarefas();
+         Uteis.print("Digite o numero da tarefa que voce concluiu");
+         int indiceFinalizado = Uteis.lerInt();
+         Tarefa tarefaConcluida = pendentes.get(indiceFinalizado);
+         tarefaConcluida.setStatus("True");
+         concluidas.add(tarefaConcluida);
+         pendentes.remove(tarefaConcluida);
+         // adicionar ao arquivo do usuario
       }
+      
    }
-         
+   public void exibirConcluidas() {
+         int index = 0;
+         for (Tarefa t : concluidas) {
+         Uteis.print("Número da tarefa: " + index++);
+         Uteis.print("Titulo: " + t.getTitulo());
+         Uteis.print("Descricao: " + t.getDescricao());
+         Uteis.print("Data de inicio: " + t.getDataInicio());
+         Uteis.print("Status: " + t.getStatus());
+         Uteis.print("-----------------------");
+    }
+
+
+}  
    }
